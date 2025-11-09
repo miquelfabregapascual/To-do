228f87d47fb2264fb01230afcfaa231122f2cf36
Miquel Fàbrega Pascual <120173496+miquelfabregapascual@users.noreply.github.com>
2025-11-09 23:58:16 +0100

    Add task factory and feature tests

---
+++ database/factories/TaskFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\\Factories;
+
+use App\\Models\\Task;
+use App\\Models\\User;
+use Illuminate\\Database\\Eloquent\\Factories\\Factory;
+
+/**
+ * @extends Factory<Task>
+ */
+class TaskFactory extends Factory
+{
+    protected $model = Task::class;
+
+    public function definition(): array
+    {
+        return [
+            'user_id' => User::factory(),
+            'title' => $this->faker->sentence(4),
+            'description' => $this->faker->optional()->paragraph(),
+            'due_date' => $this->faker->dateTimeBetween('now', '+2 weeks')->format('Y-m-d'),
+            'completed' => $this->faker->boolean(20),
+        ];
+    }
+
+    public function incomplete(): self
+    {
+        return $this->state(fn () => [
+            'completed' => false,
+        ]);
+    }
+
+    public function completed(): self
+    {
+        return $this->state(fn () => [
+            'completed' => true,
+        ]);
+    }
+}
