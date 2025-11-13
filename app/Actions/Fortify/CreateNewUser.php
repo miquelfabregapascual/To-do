4ad04144f9d558094e0edb5a122d7d9ece15befb
Miquel Fàbrega Pascual <120173496+miquelfabregapascual@users.noreply.github.com>
2025-11-13 18:30:11 +0100

    Refine planner layout sizing

--- app/Actions/Fortify/CreateNewUser.php
+++ app/Actions/Fortify/CreateNewUser.php
@@ -22,7 +22,7 @@ class CreateNewUser implements CreatesNewUsers
         Validator::make($input, [
             'name' => ['required', 'string', 'max:255', 'unique:users'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
-            'phone' => ['required', 'string', 'max:255', 'unique:users'],
+            'phone' => ['nullable', 'string', 'max:255', 'unique:users,phone'],
             'password' => $this->passwordRules(),
             'terms' => Jetstream::hasTermsAndPrivacyPolicyFeature() ? ['accepted', 'required'] : '',
         ])->validate();
@@ -30,7 +30,7 @@ class CreateNewUser implements CreatesNewUsers
         return User::create([
             'name' => $input['name'],
             'email' => $input['email'],
-            'phone' => $input['phone'],
+            'phone' => $input['phone'] ?? null,
             'password' => Hash::make($input['password']),
         ]);
     }
